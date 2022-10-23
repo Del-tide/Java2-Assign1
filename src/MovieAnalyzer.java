62c42f49169c3a326dbf7bf0b8b18e8d2debebd0
unknown <12111713@mail.sustech.edu.cn>
2022-10-23 16:33:52 +0800

    first commit

---
+++ src/MovieAnalyzer.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.Map;
+
+public class MovieAnalyzer {
+    public MovieAnalyzer(String dataset_path) {
+
+    }
+    public Map<Integer, Integer> getMovieCountByYear(){
+
+    }
+
+    public Map<String, Integer> getMovieCountByGenre(){
+
+    }
+
+    public Map<List<String>, Integer> getCoStarCount(){
+
+    }
+
+    public List<String> getTopMovies(int top_k, String by){
+
+    }
+
+    public List<String> getTopStars(int top_k, String by){
+
+    }
+
+    public List<String> searchMovies(String genre, float min_rating, int max_runtime){
+
+    }
+
+}
+
+
